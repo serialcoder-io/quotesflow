688f3ccdd61edf5e69c7802a57ec492b5c5e1d4f
omar <omaranli56@gmail.com>
2025-09-30 00:28:11 +0400

    feat: add Docker Compose file and GitHub Actions workflows (ci.yml & docker-image.yml)

--- quotesflow/settings.py
+++ quotesflow/settings.py
@@ -99,11 +99,9 @@ WSGI_APPLICATION = 'quotesflow.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 if DEBUG:
+    import dj_database_url
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.config(default=config('DATABASE_URL'))
     }
 else:
     DB_NAME = config('DB_NAME')
